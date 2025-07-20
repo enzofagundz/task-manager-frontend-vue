7a1773bb8b75c13667a7f6595106313b8e718bbd
Enzo Fagundes <enzo.fagundes014@gmail.com>
2025-07-20 13:32:28 -0300

    feat: add layouts and HomePage, update router config

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
-
+import { LayoutDefault, LayoutGuest } from '@/components'
 </script>
 
 <template>
+  <LayoutGuest v-if="$route.meta.layout === 'guest'">
+    <RouterView />
+  </LayoutGuest>
+  <LayoutDefault v-else>
+    <RouterView />
+  </LayoutDefault>
 </template>
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { default as LayoutGuest } from './layout/LayoutGuest.vue'
+export { default as LayoutDefault } from './layout/LayoutDefault.vue'
